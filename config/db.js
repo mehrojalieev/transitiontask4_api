aad4c5e6ab91d5666941ebe829265d1fd47cd08f
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-18 23:51:53 +0500

    Created

--- config/db.js
+++ config/db.js
@@ -1,12 +1,6 @@
 const Pool = require("pg").Pool
 
-// const pool = new Pool({
-//     user: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT
-// })
+
 
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
