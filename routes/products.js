3304e3ef6b4bd809c4b5517283bd349c7fed1e41
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-20 17:37:36 +0500

    I added Single-Route

--- routes/products.js
+++ routes/products.js
@@ -12,4 +12,25 @@ router.get("/all", async (req, res) => {
     }
 });
 
+// GET BY ID
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const { rows: product } = await pool.query(
+            "SELECT * FROM shop_products WHERE id = $1",
+            [id]
+        );
+
+        if (product.length === 0) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product[0]);
+    } 
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
